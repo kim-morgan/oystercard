08d42c1594d588720aefe4f7802c448bd89e1faa
Sarah <dousse.sarah@gmail.com>
2021-11-22 15:13:41 +0000

    add top_up method && pass 2nd test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,4 +3,10 @@ describe Oystercard do
     it "should have a balance of 0 when initialized" do
         expect(subject.balance).to eq 0
     end
+
+    it "should be able to top up money" do
+        card = Oystercard.new
+        card.top_up(10)
+        expect(card.balance).to eq 10
+    end
 end
